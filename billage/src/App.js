bbe29d1ab3fdb22f87ae20563396501a96aeb725
CHOI SEYOUNG <sychoi0516@naver.com>
2021-08-13 19:22:31 +0900

    카카오용 회원가입창 추가

--- billage/src/App.js
+++ billage/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignUp from './SignUp';
+import  SignUp_kakao from './SignUp_kakao';
 import { BrowserRouter,  Route } from 'react-router-dom';
 const App=()=>{
   return(
     <BrowserRouter>
                 <Route exact path="/signUp" component={SignUp}/>
+                <Route exact path="/signUp_kakao" component={SignUp_kakao}/>
     </BrowserRouter>
   );
 };
